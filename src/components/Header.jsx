c681284f2a4d938daab3fc62fd1f16bfd3814512
Murad7991 <muradsuleymanli97@gmail.com>
2023-02-21 23:02:47 +0400

    HeaderComplete

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,30 +1,45 @@
-import React from 'react'
-import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
-import SearchIcon from '@mui/icons-material/Search';
-import logoImage from '../assets/images/logo.png.webp'
+import React from "react";
+import ShoppingCartIcon from "@mui/icons-material/ShoppingCart";
+import SearchIcon from "@mui/icons-material/Search";
+import logoImage from "../assets/images/logo.png.webp";
+import "../assets/css/components/header.scss";
 const Header = () => {
   return (
     <header>
-        <div className='logo-img'>
+      <div className="main-menu container">
+        <div className="logo-img">
+          <a href="#">
             <img src={logoImage} alt="logoImage" />
+          </a>
         </div>
-        <div className='navigation'>
-            <nav>
-                <ul>
-                    <li><a href="#">Home</a></li>
-                    <li><a href="#">Shop</a></li>
-                    <li><a href="#">Blog</a></li>
-                    <li><a href="#">Pages</a></li>
-                    <li><a href="#">Contact</a></li>
-                </ul>
-            </nav>
-            <div className='nav-icons'>
-                <ShoppingCartIcon/>
-                <SearchIcon/>
-            </div>
+        <div className="navigation">
+          <nav>
+            <ul>
+              <li>
+                <a href="#">Home</a>
+              </li>
+              <li>
+                <a href="#">Shop</a>
+              </li>
+              <li>
+                <a href="#">Blog</a>
+              </li>
+              <li>
+                <a href="#">Pages</a>
+              </li>
+              <li>
+                <a href="#">Contact</a>
+              </li>
+            </ul>
+          </nav>
+          <div className="nav-icons">
+            <ShoppingCartIcon style={{ fontSize: "17px" }} />
+            <SearchIcon style={{ fontSize: "17px" }} />
+          </div>
         </div>
+      </div>
     </header>
-  )
-}
+  );
+};
 
-export default Header
+export default Header;
